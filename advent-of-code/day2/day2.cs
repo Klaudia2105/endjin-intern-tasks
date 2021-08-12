37835c4dc2d644399e27c4bba9be2ae1937cc6dd
Klaudia Guz <klaudia.guz@endjin.com>
2021-08-12 18:35:24 +0000

    Completed day2

--- advent-of-code/day2/day2.cs
+++ advent-of-code/day2/day2.cs
@@ -5,13 +5,15 @@ using System.Linq;
 
 namespace Day2
 {
-    class Hello {         
+    class Hello {   
         static void Main()
         {
             string text = @"input.txt";                                       //put contents of the file in a string 
             var lines = new List<string>();                          
 
             lines = File.ReadLines(text).ToList();                            //put text into a list
+
+            int count = 0;
 
             for(int i = 0; i < lines.Count; i++)                              
             {
@@ -28,20 +30,36 @@ namespace Day2
                 string letter = lines[i].Substring(space1, 1);  //get the letter required
 
                 int colon = lines[i].IndexOf(":") + 1;
-                string password = lines[i].Substring(colon);
-                Console.WriteLine(password);
+                string password = lines[i].Substring(colon);  //get password
                 
+                char theLetter = char.Parse(letter);
+                //now get number of characters in a string
+                int numberOfSpecifiedLetter = Other.countLetters(password, theLetter);
+                //Console.WriteLine(numberOfSpecifiedLetter);
+
+                if ((numberOfSpecifiedLetter >= min) && (numberOfSpecifiedLetter <= max))
+                {
+                    count++;
+                }
             }
-           
-            /*
-            for(int i = 0; i < lines.Count; i++)
-            {
-                int start = lines[i].IndexOf("-");                           // first find the index of your starting point
-                
-                int end = lines[i].IndexOf("\"", start);                         // then find the index of your ending point
-                
-                string goods = lines[i].Substring(start, end - start);           // then retrieve that portion of the string
-            }*/
+            Console.WriteLine(count);
+
         }
+    }
+
+    class Other
+    {
+        public static int countLetters(string password, char theLetter) 
+        {
+            int count = 0;
+            foreach (char c in password)
+            {
+                if(theLetter == c)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }      
     }
 }
