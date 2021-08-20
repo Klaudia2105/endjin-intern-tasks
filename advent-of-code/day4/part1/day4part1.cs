a1b42538a863e3653f115557fe7d0ffb027e6812
Klaudia Guz <klaudia.guz@endjin.com>
2021-08-20 14:04:45 +0000

    fixed a typo

--- advent-of-code/day4/part1/day4part1.cs
+++ advent-of-code/day4/part1/day4part1.cs
@@ -9,23 +9,23 @@ namespace day3part1
             string text = @"input.txt";                       //open the file
             string[] lines = File.ReadAllLines(text);         //store contents in an array of strings
             
-            int number_of_lines = lines.Length;
+            int number_of_lines = lines.Length;              //get the number of lines in the array of strings including the empty lines
 
-            int passport_number = GetNumberOfPassports(number_of_lines, lines);
+            int passport_number = GetNumberOfPassports(number_of_lines, lines);  //find number of passports
             
             string[] passport = new string[passport_number];
-            int[] indeces = new int[passport_number];
+            int[] indices = new int[passport_number];
             
             int j = 0;
             for(int i = 0; i < number_of_lines; i++) 
             {
                 if (lines[i] == "")  
                 {   
-                    indeces[j] = i;  //get the indeces of lines that are empty
+                    indices[j] = i;  //get the indices of lines that are empty
                     j++;
                 }
             }
-            for(int i = 0; i < indeces[0]; i++) //passport[0] = lines[0] + lines[1] + lines[2];
+            for(int i = 0; i < indices[0]; i++) //passport[0] = lines[0] + lines[1] + lines[2];
             {
                 passport[0] = passport[0] + " " + lines[i]; //get the first passport
             }
@@ -34,7 +34,7 @@ namespace day3part1
             int b = 1;       //passport[1] = lines[4] + lines[5] + lines[6] + lines[7]
             for(int i = 1; i < passport_number - 1; i++)
             {
-                for(int z = indeces[b-1]; z < indeces[b]; z++)  //get the rest of the passports except the last one because there is no empty line at the end of the text file
+                for(int z = indices[b-1]; z < indices[b]; z++)  //get the rest of the passports except the last one because there is no empty line at the end of the text file
                 {
                     passport[b] = passport[b] + " " + lines[z+1];    
                 }
@@ -44,14 +44,14 @@ namespace day3part1
             }
             for(int i = number_of_lines - 2; i < number_of_lines; i++)   //get the last passport
             {
-                passport[indeces.Length - 1] = passport[indeces.Length - 1] + " " + lines[i];
+                passport[indices.Length - 1] = passport[indices.Length - 1] + " " + lines[i];
             }
             Console.WriteLine(" ");
-            Console.WriteLine(passport[indeces.Length - 1]);
+            Console.WriteLine(passport[indices.Length - 1]);
 
             int count = 0;
 
-            for(int i = 0; i < passport_number; i++)  //check if each passport contains all reuired fields
+            for(int i = 0; i < passport_number; i++)  //check if each passport contains all required fields
             {
                 bool valid = passport[i].Contains("byr") && 
                              passport[i].Contains("iyr") && 
@@ -69,12 +69,12 @@ namespace day3part1
         }
 
 
-        static public int GetNumberOfPassports(int number_of_lines, string[] lines)
+        static public int GetNumberOfPassports(int number_of_lines, string[] lines)  //takes in the array of lines and the number of lines
         {
             int passport_number = 0;
             for(int i = 0; i < number_of_lines; i++)
             {
-                if (lines[i] == "") 
+                if (lines[i] == "")               //checks how many empty lines there are which indicates the number of passports
                 {   
                     passport_number++;
                 }
