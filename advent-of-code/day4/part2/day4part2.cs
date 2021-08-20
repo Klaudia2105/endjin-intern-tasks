da42d248e98f936470459aabe74181b1eda73928
Klaudia Guz <klaudia.guz@endjin.com>
2021-08-20 17:10:38 +0000

    completed day4 part2

--- advent-of-code/day4/part2/day4part2.cs
+++ advent-of-code/day4/part2/day4part2.cs
@@ -3,27 +3,30 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+
 
 namespace day3part1
 {
     class Hello {         
         static void Main()
         {
-            Debug.Assert(ValidateField("byr", "200") == false);
+            Debug.Assert(ValidateField("byr", "200") == false);  //some example tests to test each validating function
             Debug.Assert(ValidateField("byr", "20103") == false);
-
             Debug.Assert(ValidateField("byr", "1920") == true);
-
             Debug.Assert(ValidateField("byr", "2002") == true);
             Debug.Assert(ValidateField("byr", "2003") == false);
             Debug.Assert(ValidateField("hgt", "60in") == true);
             Debug.Assert(ValidateField("hgt", "190cm") == true);
             Debug.Assert(ValidateField("hgt", "190in") == false);
             Debug.Assert(ValidateField("hgt", "190") == false);
-
-
-
-
+            Debug.Assert(ValidateField("hcl", "#123abc") == true);
+            Debug.Assert(ValidateField("hcl", "#123abz") == false);
+            Debug.Assert(ValidateField("hcl", "123abc") == false);
+            Debug.Assert(ValidateField("ecl", "brn") == true);
+            Debug.Assert(ValidateField("ecl", "brnamb") == false);
+            Debug.Assert(ValidateField("pid", "000000001") == true);
+            Debug.Assert(ValidateField("pid", "0000000001") == false);
 
             string text = @"input.txt";                       //open the file
             string[] lines = File.ReadAllLines(text);         //store contents in an array of strings
@@ -68,6 +71,7 @@ namespace day3part1
             //Console.WriteLine(" ");
             // Console.WriteLine(passport[indeces.Length - 1]);
 
+            string[] validPassport = new string[200];   //check the passports valid from part 1
             int count = 0;
 
             for(int i = 0; i < passport_number; i++)  //check if each passport contains all reuired fields
@@ -81,21 +85,43 @@ namespace day3part1
                              passport[i].Contains("pid");
                 if(valid)
                 {
+                    validPassport[count] = passport[i];
                     count++;
                 }
             }   
-            // Console.WriteLine(count); //print how many valid passports there are
-
-            string[] fields = passport[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);  
+            int countFinal = 0;
+            //Console.WriteLine(validPassport[count]); //print how many valid passports there are
+            for(int i = 0; i < count; i++)
+            {
+                string[] fields = validPassport[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);  //split each field into seperate string
             
-            var fieldsByName = fields.ToDictionary(
-                field => field.Substring(0, 3),
-                field => field.Substring(4));
+                var fieldsByName = fields.ToDictionary(
+                    field => field.Substring(0, 3),
+                    field => field.Substring(4));
 
-            string byr = fieldsByName["byr"];
-            
-            //Console.WriteLine(byr);
-           
+                string byr = fieldsByName["byr"];
+                string iyr = fieldsByName["iyr"];
+                string eyr = fieldsByName["eyr"];
+                string hgt = fieldsByName["hgt"];
+                string hcl = fieldsByName["hcl"];
+                string ecl = fieldsByName["ecl"];  
+                string pid = fieldsByName["pid"];
+
+                bool validFinal = ValidateField("byr", byr) &&
+                                ValidateField("iyr", iyr) &&    
+                                ValidateField("eyr", eyr) &&     
+                                ValidateField("hgt", hgt) &&     
+                                ValidateField("hcl", hcl) &&     
+                                ValidateField("ecl", ecl) &&     
+                                ValidateField("pid", pid);
+                if(validFinal)
+                {
+                    countFinal++;
+                }
+            }
+               
+
+            System.Console.WriteLine(countFinal); 
         }
 
         static bool ValidateField(string type, string value)
@@ -105,6 +131,12 @@ namespace day3part1
             {
                 "byr" => ValidateByr(value),
                 "hgt" => ValidateHgt(value),
+                "iyr" => ValidateIyr(value),
+                "eyr" => ValidateEyr(value),
+                "hcl" => ValidateHcl(value),
+                "ecl" => ValidateEcl(value),
+                "pid" => ValidatePid(value),
+
                 _ => throw new ArgumentException("unsupported field type: " + type)
             };
 
@@ -112,12 +144,28 @@ namespace day3part1
             {
                 if(value.Length != 4)
                 {
-                
                     return false;
                 }
-                return IsIntInRange(value, 1920, 2002);
-                
+                return IsIntInRange(value, 1920, 2002);          
             }
+
+            bool ValidateIyr(string value)
+            {
+                if(value.Length != 4)
+                {
+                    return false;
+                }
+                return IsIntInRange(value, 2010, 2020);
+            }
+            bool ValidateEyr(string value)
+            {
+                if(value.Length != 4)
+                {
+                    return false;
+                }
+                return IsIntInRange(value, 2020, 2030);
+            }
+
             bool ValidateHgt(string value)
             {
                 if(value.EndsWith("in"))
@@ -131,13 +179,56 @@ namespace day3part1
                 return false;
             }
 
+            bool ValidateHcl(string value)
+            {
+                if(value.StartsWith("#"))
+                {
+                    if(value.Length == 7)
+                    {
+                        string afterHash = value.Substring(1);
+                        if(Regex.IsMatch(afterHash, "^[a-f0-9]*$"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            bool ValidateEcl(string value)
+            {
+                if(value == "amb" ^ 
+                   value == "blu" ^
+                   value == "brn" ^
+                   value == "gry" ^
+                   value == "grn" ^
+                   value == "hzl" ^
+                   value == "oth" 
+                   )
+                   {
+                       return true;
+                   }
+                return false;
+            }
+            bool ValidatePid(string value)
+            {
+                if(value.Length != 9)
+                {
+                    return false;
+                }
+                if(Regex.IsMatch(value, "^[0-9]*$"))
+                {
+                    return true;
+                }
+                return false;
+            }
+
             bool IsIntInRange(string value, int inclusiveMinimum, int inclusiveMaximum)
             {
                 return int.TryParse(value, out int intValue) && intValue >= inclusiveMinimum && intValue <= inclusiveMaximum;
             }
 
         }
-
 
         static public int GetNumberOfPassports(int number_of_lines, string[] lines)
         {
