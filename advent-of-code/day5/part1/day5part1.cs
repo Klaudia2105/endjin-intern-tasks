fb3f7852492005bb9fbc53b37d33d8bbf167c2e3
Klaudia Guz <klaudiaguz21051997@gmail.com>
2021-09-01 17:46:46 +0100

    completed day 5 part 1

--- advent-of-code/day5/part1/day5part1.cs
+++ advent-of-code/day5/part1/day5part1.cs
@@ -9,10 +9,7 @@ namespace day5part1
 {
     class Hello {         
         static void Main()
-        {
-            //Debug.Assert(CheckID("BFFFBBFRRR", 567) == true);  //test the code
-            //Debug.Assert(ValidateField("FFFBBBFRRR", 119) == true);
-            
+        {   
             string text = @"input.txt";                       //open the file
             string[] lines = File.ReadAllLines(text);         //store contents in an array of strings
             
@@ -21,17 +18,24 @@ namespace day5part1
             char[] f_or_b = new char[7];
             char[] r_or_l = new char[3];
 
-            string test = "FBFBBFFRLR";
+            double[] id = new double[lines.Length];
+            double[] row = new double[lines.Length];
+            double[] column = new double[lines.Length];
 
-            /*for(int j = 0; j < lines.Length; j++)
+            //string test = "BBFFBBFRLL";
+
+            for(int j = 0; j < lines.Length; j++)
             {
                 double min_row = 0;
                 double max_row = 127; 
 
+                double min_column = 0;
+                double max_column = 7;
 
                 for(int i = 0; i < 7; i++)  // get the row number
                 {
                     f_or_b[i] = lines[j][i]; 
+                    //f_or_b[i] = test[i];
                     
                     if(f_or_b[i] == 'F')     // if F take the lower half so min stays the same
                     {
@@ -41,40 +45,40 @@ namespace day5part1
                     else if(f_or_b[i] == 'B')    //if B take upper half so max stays the same
                     {
                         min_row = min_row + ((max_row - min_row) / 2); //min gets updated
-                        min_row = Math.Round(min_row);         // round up
+                        min_row = Math.Round(min_row, MidpointRounding.AwayFromZero);       // round up , the second parameter is to make sure it rounds up properly: https://docs.microsoft.com/en-us/dotnet/api/system.math.round?view=net-5.0#midpoint-values-and-rounding-conventions
                     }
                     if(min_row == max_row)
                     {
-                        Console.WriteLine(min_row);
+                        row[j] = min_row;
                     }
                 } 
-            }*/
-            double min_column = 0;
-            double max_column = 7;
-            for(int i = 0; i < 3; i++)
-            {
-                r_or_l[i] = test[7+i]; //start from char[7] up to char[9], i.e. last 3 characters
 
-                if(r_or_l[i] == 'L')     // if L take the lower half so min stays the same
+                for(int i = 0; i < 3; i++)
                 {
-                    max_column = min_column + ((max_column - min_column) / 2);  // max gets updated
-                    max_column = Math.Floor(max_column);        //round down
-                    Console.WriteLine(min_column);
-                    Console.WriteLine(max_column);
+                    r_or_l[i] = lines[j][7+i]; //start from char[7] up to char[9], i.e. last 3 characters
+
+                    if(r_or_l[i] == 'L')     // if L take the lower half so min stays the same
+                    {
+                        max_column = min_column + ((max_column - min_column) / 2);  // max gets updated
+                        max_column = Math.Floor(max_column);        //round down
+                    }
+                    else if(r_or_l[i] == 'R')    //if R take upper half so max stays the same
+                    {
+                        min_column = min_column + ((max_column - min_column) / 2); //min gets updated
+                        min_column = Math.Round(min_column, MidpointRounding.AwayFromZero);         // round up , the second parameter is to make sure it rounds up properly: https://docs.microsoft.com/en-us/dotnet/api/system.math.round?view=net-5.0#midpoint-values-and-rounding-conventions
+                    }
+                    if(min_column == max_column)
+                    {
+                        column[j] = min_column;
+                    }
                 }
-                else if(r_or_l[i] == 'R')    //if R take upper half so max stays the same
-                {
-                    min_column = min_column + ((max_column - min_column) / 2); //min gets updated
-                    min_column = Math.Round(min_column);         // round up
-                    Console.WriteLine(min_column);
-                    Console.WriteLine(max_column);
-                }
-                if(min_column == max_column)
-                {
-                    Console.WriteLine(min_column);
-                }
+                id[j] = (row[j] * 8) + column[j];
             }
-                       
+            double result = id.Max();
+
+            //double id  = (min_row * 8) + min_column;
+            System.Console.WriteLine(result);
+       
         }
     }
 }
